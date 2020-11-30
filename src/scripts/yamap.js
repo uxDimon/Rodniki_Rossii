b31f3fc67bac77cfb710ae11eb86be47702e3fce
Шелков Дмитрий <bobyorrr@gmail.com>
2020-11-30 13:37:56 +0300

    продакшон

--- src/scripts/yamap.js
+++ src/scripts/yamap.js
@@ -1,34 +1,58 @@
-ymaps.ready(init);
+// ymaps.ready(init);
+// // var myMap;
 
-function init() {
-	var myMap = new ymaps.Map(
-			"map",
-			{
-				center: [55.76, 37.64],
-				zoom: 10,
-			},
-			{
-				searchControlProvider: "yandex#search",
-			}
-		),
-		objectManager = new ymaps.ObjectManager({
-			// Чтобы метки начали кластеризоваться, выставляем опцию.
-			clusterize: true,
-			// ObjectManager принимает те же опции, что и кластеризатор.
-			gridSize: 32,
-			clusterDisableClickZoom: true,
+// function init() {
+// 	var myMap = new ymaps.Map(
+// 			"map",
+// 			{
+// 				center: [55.76, 37.64],
+// 				zoom: 10,
+// 			},
+// 			{
+// 				searchControlProvider: "yandex#search",
+// 			}
+// 		),
+// 		objectManager = new ymaps.ObjectManager({
+// 			// Чтобы метки начали кластеризоваться, выставляем опцию.
+// 			clusterize: true,
+// 			// ObjectManager принимает те же опции, что и кластеризатор.
+// 			gridSize: 32,
+// 			clusterDisableClickZoom: false,
+// 		});
+
+// 	// Чтобы задать опции одиночным объектам и кластерам,
+// 	// обратимся к дочерним коллекциям ObjectManager.
+// 	objectManager.objects.options.set("preset", "twirl#blueIcon");
+// 	objectManager.clusters.options.set("preset", "twirl#blueClusterIcons");
+// 	myMap.geoObjects.add(objectManager);
+
+// 	$.ajax({
+// 		url: "assets/scripts/data.json",
+// 	}).done(function (data) {
+// 		objectManager.add(data);
+// 	});
+
+// 	myMap.setBounds(myMap.geoObjects.getBounds(), {
+// 		checkZoomRange: true,
+// 		zoomMargin: 35,
+// 	});
+// }
+
+ymaps.ready().done(function (ym) {
+	var myMap = new ym.Map(
+		"map",
+		{
+			center: [55.751574, 37.573856],
+			zoom: 2,
+		},
+		{
+			searchControlProvider: "yandex#search",
+		}
+	);
+
+	jQuery.getJSON("assets/scripts/data.json", function (json) {
+		var geoObjects = ym.geoQuery(json).addToMap(myMap).applyBoundsToMap(myMap, {
+			checkZoomRange: true,
 		});
-
-	// Чтобы задать опции одиночным объектам и кластерам,
-	// обратимся к дочерним коллекциям ObjectManager.
-	objectManager.objects.options.set("preset", "islands#greenDotIcon");
-	objectManager.clusters.options.set("preset", "islands#greenClusterIcons");
-	myMap.geoObjects.add(objectManager);
-
-	$.ajax({
-		url: "./data.json",
-	}).done(function (data) {
-		objectManager.add(data);
 	});
-	console.log(1);
-}
+});
